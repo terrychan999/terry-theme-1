45671c179427f167ebe280e46c5fe5f21ad8659d
Terry Chan <terrychan999@gmail.com>
2021-08-14 06:50:32 +0000

    make footer username to variable

--- footer.php
+++ footer.php
@@ -2,7 +2,12 @@
         </main>
     </div><!-- content -->
     <footer id="footer">
-        <?php echo date('Y'); ?>&nbsp;Terry Chan.
+        <?php
+            // use admin public display name
+            $user_info = get_userdata(1);
+            $display_name = $user_info->display_name;
+            echo date('Y') . " " . $display_name . ".";
+        ?>
     </footer>
 </div><!-- container -->
 <?php wp_footer(); ?>
